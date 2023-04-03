51e1429e2552fdfc2a95b2586b509fdad5fe29b7
Alejandro Cambronero <cambro97@gmail.com>
2023-04-03 02:46:45 +0200

    Added Leap library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 
 #include <QCoreApplication>
+#include <stdio.h>
+#include "Leap.h"
 
+using namespace Leap;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qInfo() << "HOLA MUNDO";
 
     return a.exec();
 }
